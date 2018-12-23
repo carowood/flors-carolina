69c81b2e91375abcfd9c142bf2099e65cd568450
Caroline Wood <carowood@gmail.com>
2018-12-23 20:25:46 +0100

    Add form elements

--- src/translations/LocaleContext.js
+++ src/translations/LocaleContext.js
@@ -1,3 +1,8 @@
 import React from "react";
 
 export const LocaleContext = React.createContext();
+
+// This is our context object.
+
+// When React renders a component that subscribes to this context object,
+// it will read the context value from the closest matching Provider.
