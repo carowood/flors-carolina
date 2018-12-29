656144f8ff698e87b869b15d08f7a22a1d3b327d
Caroline Wood <carowood@gmail.com>
2018-12-29 23:15:14 +0100

    Add flexbox

--- src/components/PressGrid/PressGridItem.js
+++ src/components/PressGrid/PressGridItem.js
@@ -2,11 +2,17 @@ import React from "react";
 
 export default class PressGridItem extends React.Component {
   render() {
+    console.log("presspage", this.props.url);
     return (
       <div className="press-grid-item">
-        <h1>{this.props.title}</h1>
-        <p>{this.props.bodytext}</p>
-        <a href={this.props.url} target="_blank" rel="noopener noreferrer">
+        <h1 className="press-grid-item__title">{this.props.title}</h1>
+        <p className="press-grid-item__bodytext">{this.props.bodytext}</p>
+        <a
+          href={this.props.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="press-grid-item__url"
+        >
           {this.props.readMoreLink}
         </a>
       </div>
