f64f8b9e95a10b0b71fcb2386aa94fa1736cd2eb
Caroline Wood <carowood@gmail.com>
2019-01-01 22:29:04 +0100

    Make variables clearer

--- src/components/PressGrid/PressGridItem.js
+++ src/components/PressGrid/PressGridItem.js
@@ -4,18 +4,21 @@ export default class PressGridItem extends React.Component {
   render() {
     let url = this.props.url;
     let item = this.props.children;
+    let title = item[0];
+    let bodyText = item[1];
+    let linkText = item[2];
 
     return (
       <div className="press-grid-item">
-        <h1 className="press-grid-item__title">{item[0]}</h1>
-        <p className="press-grid-item__bodytext">{item[1]}</p>
+        <h1 className="press-grid-item__title">{title}</h1>
+        <p className="press-grid-item__bodytext">{bodyText}</p>
         <a
           href={url}
           target="_blank"
           rel="noopener noreferrer"
           className="press-grid-item__url"
         >
-          {item[2]}
+          {linkText}
         </a>
       </div>
     );
