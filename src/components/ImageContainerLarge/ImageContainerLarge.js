0a24a4f8e81cdddeb2a57969e530e8b85de3bf44
Caroline Wood <carowood@gmail.com>
2019-01-11 20:33:38 +0100

    Add links to homepage buttons

--- src/components/ImageContainerLarge/ImageContainerLarge.js
+++ src/components/ImageContainerLarge/ImageContainerLarge.js
@@ -3,11 +3,10 @@ import TextBox from "../TextBox/TextBox";
 import Translate from "../../translations/Translate";
 import HomeContent from "../HomeContent/HomeContent";
 import BackgroundImageContainer from "../BackgroundImageContainer/BackgroundImageContainer";
+import { Link } from "react-router-dom";
 
 export default class ImageContainerLarge extends React.Component {
   render() {
-    // let text =
-    //   "“Les flors es regalen des del moment que neixes, fins al moment que faltes. Moments molt feliços i moments molt tristos. Però entre aquest període, que és la vida, la flor sempre ens acompanya” ";
     return (
       <div className="container">
         <div className="container__top">
@@ -15,27 +14,24 @@ export default class ImageContainerLarge extends React.Component {
           <TextBox>
             <Translate string={"home-top-section-bodytext"} />
           </TextBox>
-          {/* <div id="backgroundchange">
-            <div className="backgroundimg" id="back1" />
-            <div className="backgroundimg" id="back2" />
-            <div className="backgroundimg" id="back3" />
-            <div className="backgroundimg" id="back4" />
-            <div className="backgroundimg" id="back5" />
-          </div> */}
         </div>
         <HomeContent class={"middle"} image1={""} image2={""}>
           <Translate string={"home-mid-section.title"} />
           <Translate string={"home-mid-section.alt-tag"} />
           <Translate string={"home-mid-section.alt-tag"} />
           <Translate string={"home-mid-section.bodytext"} />
-          <Translate string={"home-mid-section.buttontext"} />
+          <Link to="/ourstory" onClick={this.props.click}>
+            <Translate string={"home-mid-section.buttontext"} />
+          </Link>
         </HomeContent>
         <HomeContent class={"bottom"} image1={""} image2={""}>
           <Translate string={"home-last-section.title"} />
           <Translate string={"home-last-section.alt-tag"} />
           <Translate string={"home-last-section.alt-tag"} />
           <Translate string={"home-last-section.bodytext"} />
-          <Translate string={"home-last-section.buttontext"} />
+          <Link to="/services" onClick={this.props.click}>
+            <Translate string={"home-last-section.buttontext"} />
+          </Link>
         </HomeContent>
       </div>
     );
