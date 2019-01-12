c85fe4bc54e44ac0950a2cafbd643456fe9d6ab1
Caroline Wood <carowood@gmail.com>
2019-01-12 22:37:55 +0100

    Add scroll iconcomponent

---
+++ src/components/ScrollButton/ScrollButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import icon from "../../assets/icons/scroll-down-icon.png";
+
+export default class ScrollButton extends React.Component {
+  render() {
+    return (
+      <button className="scroll-down-button">
+        <img className="scroll-down-icon" src={icon} alt="" />
+      </button>
+    );
+  }
+}
--- src/components/TextBox/TextBox.js
+++ src/components/TextBox/TextBox.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BorderLeft from "../../assets/border/image-corner-left.svg";
 import BorderRight from "../../assets/border/image-corner-right.svg";
+import ScrollButton from "../ScrollButton/ScrollButton";
 
 export default class TextBox extends React.Component {
   render() {
@@ -10,6 +11,7 @@ export default class TextBox extends React.Component {
           <img src={BorderLeft} alt="border" className="image-border-left" />
           {/* <p className="para">{this.props.text}</p> */}
           <p className="para">{this.props.children}</p>
+          <ScrollButton />
           <img src={BorderRight} alt="border" className="image-border-right" />
         </div>
       </div>
