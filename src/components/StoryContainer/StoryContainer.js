60024f12494bd369123de95845f49ba49d402052
Caroline Wood <carowood@gmail.com>
2019-01-23 21:30:46 +0100

    Add PNG files to ourstory page, remove unused icons

--- src/components/StoryContainer/StoryContainer.js
+++ src/components/StoryContainer/StoryContainer.js
@@ -1,11 +1,12 @@
 import React from "react";
 import StoryItem from "./StoryItem";
+import StoryItemImage from "../../assets/images/ourstory/ourstory-main.png";
 
 export default class StoryContainer extends React.Component {
   render() {
     return (
       <div>
-        <StoryItem image="/images/shopfront.jpg" />
+        <StoryItem image={StoryItemImage} />
       </div>
     );
   }
