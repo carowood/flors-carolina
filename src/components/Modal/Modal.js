d46044c6c23677880ec668acbbbb20f3a43563fb
Caroline Wood <carowood@gmail.com>
2019-01-27 20:23:08 +0100

    Fix button on modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -7,9 +7,14 @@ export default class Modal extends React.Component {
       <div className="modal">
         <button className="modal__closeicon" onClick={this.props.hideModal} />
         <div className="modal__content">{this.props.children}</div>
-        <button className="modal__closebutton" onClick={this.props.hideModal}>
+        <div
+          className="modal__closebutton"
+          id="closebutton"
+          onClick={this.props.hideModal}
+        >
           <Translate string={"modal.close"} />
-        </button>
+        </div>
+        <div className="modal__spacer" />
       </div>
     );
 
