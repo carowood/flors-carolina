66de8c47d0b32ee204232a93825cc3dbe66878ce
Caroline Wood <carowood@gmail.com>
2019-01-27 17:50:56 +0100

    Add alt tags homepage images

--- src/components/HomeBottom/HomeBottom.js
+++ src/components/HomeBottom/HomeBottom.js
@@ -4,12 +4,28 @@ export default class HomeBottom extends React.Component {
   render() {
     let item = this.props.children;
     let title = item[0];
-    let altTag1 = item[1];
-    let altTag2 = item[2];
     let bodyText1 = item[3];
     let bodyText2 = item[4];
     let buttonText1 = item[5];
     let buttonText2 = item[6];
+
+    let altTag1 = "";
+    if (this.props.lang === "en") {
+      altTag1 = "A bridal bouquet";
+    } else if (this.props.lang === "cat") {
+      altTag1 = "Un ram de núvia";
+    } else {
+      altTag1 = "Un ramo de novia";
+    }
+
+    let altTag2 = "";
+    if (this.props.lang === "en") {
+      altTag2 = "Table decorations";
+    } else if (this.props.lang === "cat") {
+      altTag2 = "Decoracions de taula";
+    } else {
+      altTag2 = "Decoraciones de mesa";
+    }
 
     return (
       <div className="container__bottom">
