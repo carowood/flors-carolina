c44f9c36a4f1bef25231752836ed367273fe329e
Caroline Wood <carowood@gmail.com>
2019-01-27 18:19:00 +0100

    Add alt tags

--- src/components/Bouquets/Bouquets.js
+++ src/components/Bouquets/Bouquets.js
@@ -8,41 +8,49 @@ export default function Bouquets() {
   return (
     <>
       <div className="container_bouquets">
-
-          <div className="container_title">
-            <h2>
+        <div className="container_title">
+          <h2>
             <Translate string={"services.bheader"} />
-            </h2>
-          </div>
+          </h2>
+        </div>
 
-          <div className="container_description1">
-            <p>
-            <Translate string={"services.bdescription"}/>
-            </p>
-          </div>
-          <div className="container_description2">
-              <ul>
-                <li><Translate string={"services.boption1"} /></li>
-                <li><Translate string={"services.boption2"} /></li>
-                <li><Translate string={"services.boption3"} /></li>
-                <li><Translate string={"services.boption4"} /></li>
-                <li><Translate string={"services.boption5"} /></li>
-              </ul>
-          </div>
+        <div className="container_description1">
+          <p>
+            <Translate string={"services.bdescription"} />
+          </p>
+        </div>
+        <div className="container_description2">
+          <ul>
+            <li>
+              <Translate string={"services.boption1"} />
+            </li>
+            <li>
+              <Translate string={"services.boption2"} />
+            </li>
+            <li>
+              <Translate string={"services.boption3"} />
+            </li>
+            <li>
+              <Translate string={"services.boption4"} />
+            </li>
+            <li>
+              <Translate string={"services.boption5"} />
+            </li>
+          </ul>
+        </div>
 
-          <div className="container_imgfour">
-             <img src={Imagesfour}  alt="Imagesix"/>
-          </div>
+        <div className="container_imgfour">
+          <img src={Imagesfour} alt="Floral arrangement" />
+        </div>
 
-          <div className="container_imgfive">
-            <img src={Imagesfive}  alt=""/>
-          </div>
+        <div className="container_imgfive">
+          <img src={Imagesfive} alt="Birthday floral arrangement" />
+        </div>
 
-          <div className="container_imgeight">
-            <img src={Imageseight}  alt=""/>
-          </div>
+        <div className="container_imgeight">
+          <img src={Imageseight} alt="Table centerpiece" />
+        </div>
       </div>
     </>
-
   );
 }
--- src/components/Pillows/Pillows.js
+++ src/components/Pillows/Pillows.js
@@ -3,29 +3,27 @@ import Translate from "../../translations/Translate";
 import Imagesix from "../../assets/images/services/Wsix.png";
 import Imageseven from "../../assets/images/services/Wseven.png";
 
-
 export default function Pillows() {
   return (
     <>
       <div className="cp_pillows">
-          <div className="cp_title">
-            <h2>
-              <Translate string={"services.pheader"} />
-            </h2>
-          </div>
-          <div className="cp_description">
-            <p>
-              <Translate string={"services.pdescription"}/>
-            </p>
-          </div>
-          <div className="cp_image6">
-              <img src={Imagesix}  alt="Imagesix" />
-          </div>
-          <div className="cp_image7">
-              <img src={Imageseven}  alt="Imageseven" />
-          </div>
+        <div className="cp_title">
+          <h2>
+            <Translate string={"services.pheader"} />
+          </h2>
+        </div>
+        <div className="cp_description">
+          <p>
+            <Translate string={"services.pdescription"} />
+          </p>
+        </div>
+        <div className="cp_image6">
+          <img src={Imagesix} alt="Bright floral wreath" />
+        </div>
+        <div className="cp_image7">
+          <img src={Imageseven} alt="Seasonal wreath" />
+        </div>
       </div>
     </>
-
   );
 }
